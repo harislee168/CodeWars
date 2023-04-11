7010258c366480f73c3f106f3547977ee0c339c9
harislee168 <haris.lee168@gmail.com>
2023-04-11 15:27:38 +0800

    Create phone number code wars

--- CreatePhoneNumber.py
+++ CreatePhoneNumber.py
@@ -1,12 +1,2 @@
-def create_phone_number(n):
-    #your code here
-    phone_num = '('
-    for index in range(0,3):
-        phone_num+=str(n[index])
-    phone_num += ') '
-    for index in range(3,6):
-        phone_num+=str(n[index])
-    phone_num += '-'
-    for index in range(6,10):
-        phone_num+=str(n[index])
-    return phone_num
+heap = [0] * 5
+print('heap:', heap)
