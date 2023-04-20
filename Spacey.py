35d5b072c5314c13ea2c34e69db6b121221c58bc
harislee168 <haris.lee168@gmail.com>
2023-04-20 13:55:01 +0800

    Change the spacey solution to become 1 loop

--- Spacey.py
+++ Spacey.py
@@ -1,11 +1,8 @@
 def spacey(array):
     len_array = len(array)
     return_list = []
-    
+
     for index in range(len_array):
-        return_word = ''
-        for second_index in range(index+1):
-            return_word += array[second_index]
+        return_word = ''.join(array[:index+1])
         return_list.append(return_word)
     return return_list
-    
