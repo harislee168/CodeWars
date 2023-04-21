d88ed9785efb2e89356630730f0bbfe3202bbc8b
harislee168 <haris.lee168@gmail.com>
2023-04-21 14:19:37 +0800

    Duplicate Encode using collections solution

--- DuplicateEncode.py
+++ DuplicateEncode.py
@@ -1,9 +1,12 @@
+from collections import Counter
 def duplicate_encode(word):
     lower_word = word.lower()
+    counter = Counter(lower_word)
     return_string = ''
     for char in lower_word:
-        if lower_word.count(char) > 1:
+        if counter[char] > 1:
             return_string += ')'
         else:
             return_string += '('
-    return return_string
+    
+    return return_string        
