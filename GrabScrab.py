d00de37b7916a64898104d9b4efd1e9461c5f4cd
harislee168 <haris.lee168@gmail.com>
2023-04-21 15:46:48 +0800

    GrabScrab string comparison solution

--- GrabScrab.py
+++ GrabScrab.py
@@ -1,13 +1,8 @@
-from collections import Counter
 def grabscrab(said, possible_words):
-    #create list to store the words of match result
-    word_list = []
-    #use counter to store each char and the no of occurence to match the list
-    said_counter = Counter(said)
+    format_said = sorted(said.lower())
+    return_list = []
     for word in possible_words:
-        #store the word into the counter as well to check against said_counter
-        word_counter = Counter(word)        
-        if said_counter == word_counter:
-            word_list.append(word)
-    
-    return word_list
+        format_word = sorted(word.lower())
+        if format_said == format_word:
+            return_list.append(word)
+    return return_list
